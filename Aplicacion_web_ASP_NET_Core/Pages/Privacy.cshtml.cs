696af9296411b7313746e47f6239b49e8bfae0ee
JJ <juanfragamadrid@gmail.com>
2022-04-11 18:52:38 +0200

    Agregar archivos de proyecto.

---
+++ Aplicacion_web_ASP_NET_Core/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aplicacion_web_ASP_NET_Core.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Aplicacion_web_ASP_NET_Core_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aplicacion_web_ASP_NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
