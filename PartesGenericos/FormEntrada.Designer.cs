55442ea0a95651d88bb4ff2defb4790988ffe60d
JJ <juanfragamadrid@gmail.com>
2022-04-11 19:45:20 +0200

    Primero

--- PartesGenericos/FormEntrada.Designer.cs
+++ PartesGenericos/FormEntrada.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PartesGenericos
 {
-    partial class Form1
+    partial class FormEntrada
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,13 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(800, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(0, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(800, 350);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
@@ -44,5 +70,8 @@
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
     }
 }
---
+++ PartesGenericos/FormEntrada.cs
@@ -0,0 +1,20 @@
+namespace PartesGenericos
+{
+    public partial class FormEntrada : Form
+    {
+        public FormEntrada()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
